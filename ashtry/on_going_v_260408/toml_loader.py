ba7df636cc46c3ccf4a3c370adc6e4b7adff9f6b
Husky <duskyhuskyht@gmail.com>
2026-04-21 21:56:29 +0900

    Update toml_loader.py

--- ashtry/on_going_v_260408/toml_loader.py
+++ ashtry/on_going_v_260408/toml_loader.py
@@ -1,7 +1,15 @@
 import os
 import sys
-import toml
 from dataclasses import dataclass
+
+def ensure_toml():
+    try:
+        return importlib.import_module('toml')
+    except ImportError:
+        subprocess.check_call([sys.executable, "-m", "pip", "install", "toml", "--user"])
+        return importlib.import_module('toml')
+
+toml = ensure_toml()
 
 
 FILE_PATH = ".config/FreeCAD/waffle.toml"
@@ -26,11 +34,17 @@ class toml_consts:
             return self
         try:
             toml_data = toml.load(FILE_PATH)
+            sheet_info_dic = toml_data.get(('sheet_information',{})
+            config_data = toml_consts(**{
+            k: v for k, v in sheet_info_dic.items() 
+            if k in toml_data.__annotations__
+        })
+            
             print(toml_data)
             App.Console.PrintMessage(toml_data)
         except Exception as e:
             print(f"failed to load toml file. continue with default constants.: {e}")
-            return
+            return self
 
     def config_updater(self):
         toml_data = toml.load(FILE_PATH)
