0e936c73003d6ec67c606587644af204cedbdcd0
Husky <duskyhuskyht@gmail.com>
2026-04-21 21:36:11 +0900

    Add files via upload

---
+++ ashtry/on_going_v_260408/toml_loader.py
@@ -0,0 +1,39 @@
+import os
+import sys
+import toml
+from dataclasses import dataclass
+
+
+FILE_PATH = ".config/FreeCAD/waffle.toml"
+
+
+@dataclass
+class toml_consts:
+    sheet_thickness: float = 2.1
+    min_interval: float = 2.0
+    outer_interval: float = 10.0
+    cube_z_offset: float = -32.0
+    cube_total_height: float = 100.0
+    safety_height: float = 5.0
+    cube_margin: float = 10.0
+    dxf_sheets_gap: float = 10.0
+
+    def config_loader(self):
+        # toml_data = toml.load(FILE_PATH)
+
+        if not os.path.exists(FILE_PATH):
+            print("file is not exist ...")
+            return self
+        try:
+            toml_data = toml.load(FILE_PATH)
+            print(toml_data)
+            App.Console.PrintMessage(toml_data)
+        except Exception as e:
+            print(f"failed to load toml file. continue with default constants.: {e}")
+            return
+
+    def config_updater(self):
+        toml_data = toml.load(FILE_PATH)
+
+        print(toml_data)
+        App.Console.PrintMessage(toml_data)
